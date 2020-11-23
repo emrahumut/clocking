e3584438e2a196f46f0b11330f9f13eac89d5ff9
Emrah Umut KOÇ <emrahumutkoc@gmail.com>
2020-11-23 17:36:08 +0300

    edited

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ function setDate() {
     // Second Hand
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360);
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
     
     // Min Hand
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + 90;
+    const minsDegrees = ((mins / 60) * 360);
     minHand.style.transform = `rotate(${minsDegrees}deg)`
 
     // Hour Hand
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360 ) + 90 ;
+    const hoursDegrees = ((hours / 12) * 360 );
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
