73e8ace9ea7fb0e1a64110650988a5d9b8a6c3ad
Emrah Umut KOÇ <emrahumutkoc@gmail.com>
2020-11-23 17:33:51 +0300

    hands styles is updated

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
+    // Second Hand
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-    // console.log(secondHand.style.transform);
     
+    // Min Hand
+
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
     minHand.style.transform = `rotate(${minsDegrees}deg)`
-    // console.log(minHand.style.transform);    
+
+    // Hour Hand
 
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360 ) + 90 ;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`
-
-
 }
 
 setInterval(setDate,1000);
